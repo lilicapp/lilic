bc7fc8a6f2f9f61f74d87fa5d82283860905468d
Javier <javiersb1997@gmail.com>
2019-07-01 15:54:07 +0100

    Crear rama dev

--- app/pages/SignUp.page.js
+++ app/pages/SignUp.page.js
@@ -5,7 +5,6 @@ import SignUp from '../components/SignUp/SignUp.component';
 
 class SignUpPage extends Component {
   render () {
-    console.log(this.props);
     return (
       <SignUp navigation={this.props.navigation}/>
     );
